0ba52dabbe9535571659a5eff7243d26b8a9b05b
wesley <501134@student.saxion.nl>
2022-07-17 23:54:06 +0200

    setup gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,21 @@
-import sveltePreprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+  preprocess: preprocess(),
+
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist/assets',
+      fallback: null
+    })
+  }
+};
+
+export default config;
