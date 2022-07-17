93256997c1fbd609778ec90a932c0d52d47f656d
wesley <501134@student.saxion.nl>
2022-07-18 00:42:11 +0200

    initial gh-pages commit

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm run build
 # Commit and push the changes
 git add .
 git commit -m "initial gh-pages commit"
-git push origin gh-pages
+git push -f origin gh-pages
 
 # Deploy the code with the gh-pages module
 node ./gh-pages.js
